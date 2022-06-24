7361707d24d985d8a8499b954cbf20ec3ce05a12
ilusha2274 <ilya.Shamanin@yandex.ru>
2022-06-24 12:02:28 +0300

    сделал оформление

---
+++ src/main/java/com/task5/task5/spring/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.task5.task5.spring;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(value = "controllers")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+}
